2c543f747123a62ecd1739a3ea8774878ad3b67b
mdhazar <m.derya.hazar@gmail.com>
2024-11-11 18:47:21 +0300

    Answer shouldWakeUp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,26 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+        return isBarking && ((clock < 8) || (clock >= 20));
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       return true;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        return true;
     }
 
     public static double area(double width, double height) {
-       
+       return 1.1;
     }
 
     public static double area(double radius) {
-       
+       return 1.2;
     }
 }
